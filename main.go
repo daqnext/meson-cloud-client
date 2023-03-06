ca9d5bdb4af6236ccc5e4faae7acf6e2eda2d96b
Jaimin Pan <jaimin.pan@gmail.com>
2023-03-06 11:04:52 +0800

    support vendor ipfs and config in workding dir and binary dir

--- main.go
+++ main.go
@@ -3,18 +3,32 @@ package main
 import (
     "context"
     "log"
+    "os"
+    "path/filepath"
 
     "daqnext/meson-cloud-client/api"
     "daqnext/meson-cloud-client/daemon"
     "daqnext/meson-cloud-client/logger"
+    "daqnext/meson-cloud-client/utils"
+
     "github.com/spf13/viper"
 )
 
+var BINARY_DIR string
+
 func main() {
+    ex, err := os.Executable()
+    if err != nil {
+        panic(err)
+    }
+    BINARY_DIR = filepath.Dir(ex)
+
     // Read Config
-    viper.SetConfigName("config") // name of config file (without extension)
-    viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
-    viper.AddConfigPath(".")      // path to look for the config file in
+    viper.SetConfigName("config")   // name of config file (without extension)
+    viper.SetConfigType("yaml")     // REQUIRED if the config file does not have the extension in the name
+    viper.AddConfigPath(".")        // path to look for the config file in
+    viper.AddConfigPath(BINARY_DIR) // path to look for the config file in
+
     //viper.AddConfigPath("$HOME/.appname") // call multiple times to add many search paths
     if err := viper.ReadInConfig(); err != nil {
         log.Panicln("Failed to read the config", err.Error())
@@ -33,6 +47,7 @@ func main() {
     logger.RegisterLogger(logLevel)
     defer logger.L.Sync()
 
+    ipfsCfg.IpfsCmd, err = parserIpfsCmd(ipfsCfg.IpfsCmd)
     logger.L.Debugln("cmd", ipfsCfg.IpfsCmd, "dataRoot", ipfsCfg.IpfsDataRoot, "queryUrl", queryUrl, "token", token)
 
     ctx, cancelFunc := context.WithCancel(context.Background())
@@ -47,4 +62,17 @@ func main() {
     // Api Jobs
     apiMgr := api.NewApiMgr(queryUrl, token, ipfsDaemon)
     apiMgr.Run()
+}
+
+func parserIpfsCmd(path string) (string, error) {
+
+    relpath, exist, err := utils.RelPathAndCheck(BINARY_DIR, path)
+    if err != nil {
+        logger.L.Panicw("Failed to read ipfs cmd", "err", err.Error())
+    }
+    if !exist {
+        logger.L.Panicln("ipfs cmd not exists", path)
+    }
+
+    return relpath, err
 }
